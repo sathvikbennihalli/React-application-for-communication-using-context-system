989a806bb660492cc9acb453cbe6980d9455dd68
Sathvik Bennihalli <sathvikbennihalli@Sathviks-MacBook-Air.local>
2023-04-26 10:38:08 +0530

    context system

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import './index.css'
+import { Provider } from "./context/books";
+import "./index.css";
 
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider>
+    <App />
+  </Provider>
+);
